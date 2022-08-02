ab47d332831f65d2d87eabf9c1023597037068c1
Tibor22 <tothtibitravel@gmail.com>
2022-08-03 00:08:09 +0100

    employee update functionality works

--- src/components/Profiles/EmployeeProfile.js
+++ src/components/Profiles/EmployeeProfile.js
@@ -1,67 +1,160 @@
 import DateFormatter from '../../utils/DateFormatter';
 import profileIMG from '../../assets/profileIMG.png';
 import UploadImage from '../UploadImage/UploadImage';
+import { DatePicker } from 'react-rainbow-components';
 
-export default function EmployeeProfile({ user, setUser }) {
+export default function EmployeeProfile({
+	user,
+	setUser,
+	handleClick,
+	handleChange,
+	checkUser,
+	uploadImage,
+	setUploadProfileImage,
+	uploadProfileImage,
+}) {
 	return (
 		<div className='profile'>
 			<div className='profile-img'>
 				<div className='profile-img-container'>
-					<img src={profileIMG} alt='' />
+					<img
+						src={
+							`http://localhost:4000/images/${user.employeeProfile.profileImgUrl}` || {
+								profileIMG,
+							}
+						}
+						alt=''
+					/>
 					<div className='profile-img-container--rating'>
 						<span>Rating:&nbsp;</span> {user.employeeProfile.rating || 0}%
 					</div>
 				</div>
 				<div className='profile-upload-image'>
-					<UploadImage />
+					<UploadImage
+						uploadImage={uploadImage}
+						setFormData={setUploadProfileImage}
+						formData={uploadProfileImage}
+					/>
+					<span onClick={uploadImage} className='upload-image'>
+						Save image
+					</span>
 				</div>
 			</div>
 			<div className='profile-details'>
 				<div className='profile-controller'>
 					<span>First Name</span>
 					<div className='profile-controller--details'>
-						{user.employeeProfile.firstName}
+						{!checkUser['firstName'] ? (
+							user.employeeProfile.firstName
+						) : (
+							<input
+								className='updateUser'
+								value={user.employeeProfile.firstName}
+								type='text'
+								onChange={handleChange}
+								name='firstName'
+							/>
+						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='firstName'>
+						{!checkUser['firstName'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 				<div className='profile-controller'>
 					<span>Last Name</span>
 					<div className='profile-controller--details'>
-						{user.employeeProfile.lastName}
+						{!checkUser['lastName'] ? (
+							user.employeeProfile.lastName
+						) : (
+							<input
+								value={user.employeeProfile.lastName}
+								onChange={handleChange}
+								className='updateUser'
+								type='text'
+								name='lastName'
+							/>
+						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='lastName'>
+						{!checkUser['lastName'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 				<div className='profile-controller'>
 					<span>Date of Birth</span>
 					<div className='profile-controller--details'>
-						{DateFormatter(user.employeeProfile.DoB)}
+						{!checkUser['DoB'] ? (
+							DateFormatter(user.employeeProfile.DoB)
+						) : (
+							<DatePicker
+								onChange={(value) => handleChange(value)}
+								value={user.employeeProfile.DoB || new Date()}
+							/>
+						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='DoB'>
+						{!checkUser['DoB'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 				<div className='profile-controller'>
 					<span>Address</span>
 					<div className='profile-controller--details'>
-						{user.employeeProfile.address}
+						{!checkUser['address'] ? (
+							user.employeeProfile.address
+						) : (
+							<input
+								value={user.employeeProfile.address}
+								onChange={handleChange}
+								className='updateUser'
+								type='text'
+								name='address'
+							/>
+						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='address'>
+						{!checkUser['address'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 				<div className='profile-controller'>
 					<span>Phone Number</span>
 					<div className='profile-controller--details'>
-						{user.employeeProfile.phoneNum || (
-							<span id='profile-details-empty'>Please fill out</span>
+						{!checkUser['phoneNum'] ? (
+							user.employeeProfile.phoneNum || (
+								<span id='profile-details-empty'>Please fill out</span>
+							)
+						) : (
+							<input
+								value={user.employeeProfile.phoneNum}
+								onChange={handleChange}
+								className='updateUser'
+								type='text'
+								name='phoneNum'
+							/>
 						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='phoneNum'>
+						{!checkUser['phoneNum'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 				<div className='profile-controller'>
 					<span>Bio</span>
-					<div className='profile-controller--details bio'>
-						{(user.employeeProfile.bio && <span> Read More</span>) || (
-							<span id='profile-details-empty'>Please fill out</span>
+					<div className='profile-controller--details'>
+						{!checkUser['bio'] ? (
+							user.employeeProfile.bio || (
+								<span id='profile-details-empty'>Please fill out</span>
+							)
+						) : (
+							<input
+								value={user.employeeProfile.bio}
+								onChange={handleChange}
+								className='updateUser'
+								type='text'
+								name='bio'
+							/>
 						)}
 					</div>
-					<div className='btn-edit'>Edit</div>
+					<div onClick={handleClick} className='btn-edit' name='bio'>
+						{!checkUser['bio'] ? 'Edit' : 'Save'}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -8,20 +8,88 @@ export default function Profile() {
 	const currUser = JSON.parse(localStorage.getItem('user'));
 	const [user, setUser] = useState(null);
 	const [isLoading, setIsLoading] = useState(null);
-	console.log(currUser);
+	const [uploadProfileImage, setUploadProfileImage] = useState(null);
+	const [checkUser, setCheckUser] = useState({
+		firstName: false,
+		lastName: false,
+	});
 
 	useEffect(() => {
 		if (currUser) {
 			setIsLoading(true);
 			const getUser = async () => {
 				const res = await client.get(`/user/find/${currUser.userId}`);
-				console.log(res.data);
 				setUser(res.data);
 				setIsLoading(false);
 			};
 			getUser();
 		}
 	}, []);
+
+	const handleChange = async (e) => {
+		console.log(e);
+		if (typeof e.getMonth === 'function') {
+			const value = e.toISOString();
+
+			setUser({
+				...user,
+				employeeProfile: { ...user.employeeProfile, ['DoB']: value },
+			});
+		} else {
+			const { value, name } = e.target;
+			console.log(value, name);
+			setUser({
+				...user,
+				employeeProfile: { ...user.employeeProfile, [name]: value },
+			});
+		}
+	};
+
+	const handleClick = async (e) => {
+		const name = e.target.getAttribute('name');
+		setCheckUser({ ...checkUser, [name]: !checkUser[name] });
+
+		console.log(e.target.innerText === 'Save');
+
+		const dataToSend = { [name]: user.employeeProfile[name] };
+
+		console.log(dataToSend);
+
+		if (e.target.innerText === 'Save') {
+			const res = await client.patch(
+				`/user/profile/update/${currUser.userId}`,
+				dataToSend
+			);
+		}
+	};
+
+	console.log(user);
+
+	const uploadImage = async () => {
+		const myRenamedFile = new File(
+			[uploadProfileImage.file],
+			`${uploadProfileImage.file.name}${String(Math.random())}.png`
+		);
+		let formData1 = new FormData();
+		console.log('RENAMEDFILE: ', myRenamedFile);
+		formData1.append('file', myRenamedFile);
+
+		const imgToSend = {
+			profileImgUrl: myRenamedFile.name,
+		};
+
+		console.log('IMGTOSEND', imgToSend);
+		const postRes = await client.patch(
+			`/user/profile/update/${currUser.userId}`,
+			imgToSend
+		);
+		console.log('REACH THIS');
+		console.log('RENAMEDFILE: ', myRenamedFile);
+		const response = await fetch('http://localhost:4000/v1/user/image', {
+			method: 'POST',
+			body: formData1,
+		});
+	};
 
 	return (
 		<div className='profile-outer-container'>
@@ -35,7 +103,16 @@ export default function Profile() {
 					<EmployerProfile user={user} setUser={setUser} />
 				)}
 				{currUser.type === 'employee' && user && (
-					<EmployeeProfile user={user} setUser={setUser} />
+					<EmployeeProfile
+						handleClick={handleClick}
+						user={user}
+						setUser={setUser}
+						checkUser={checkUser}
+						handleChange={handleChange}
+						uploadImage={uploadImage}
+						uploadProfileImage={uploadProfileImage}
+						setUploadProfileImage={setUploadProfileImage}
+					/>
 				)}
 			</div>
 		</div>
